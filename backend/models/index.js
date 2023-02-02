f185dae01c49a75c955b1f34177cb8a804486b45
[AI6/1반] 조해정 <hj6elice@gmail.com>
2023-02-03 01:17:35 +0900

    feat: 카테고리 모델 추가

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import UserSchema from "./schemas/user";
 import ProductSchema from "./schemas/product";
+import CategorySchema from "./schemas/category";
 
 export const User = mongoose.model("User", UserSchema);
 export const Product = mongoose.model("Product", ProductSchema);
+export const Category = mongoose.model("Category", CategorySchema);
