56e7c2fa1bd984c278c7c946f81b0a748d11d23c
kshoon <kshoon0518@gmail.com>
2023-02-04 02:56:22 +0900

    deleted:    frontend/.gitignore deleted:    frontend/README.md deleted:    frontend/package-lock.json deleted:    frontend/package.json deleted:    frontend/public/favicon.ico deleted:    frontend/public/index.html deleted:    frontend/public/logo192.png deleted:    frontend/public/logo512.png deleted:    frontend/public/manifest.json deleted:    frontend/public/robots.txt deleted:    frontend/src/App.css deleted:    frontend/src/App.js deleted:    frontend/src/App.test.js deleted:    frontend/src/components/Header.js deleted:    frontend/src/components/Nav.js deleted:    frontend/src/index.css deleted:    frontend/src/index.js deleted:    frontend/src/pages/LoginPage/Login.js deleted:    frontend/src/pages/LoginPage/styled.js eleted:    frontend/src/pages/MainPage/Main.js deleted:    frontend/src/pages/MainPage/styled.js deleted:    frontend/src/pages/OrderPage/Order.js deleted:    frontend/src/pages/OrderPage/styled.js eleted:    frontend/src/pages/SignupPage/Signup.js deleted:    frontend/src/pages/SignupPage/styled.js eleted:    frontend/src/reportWebVitals.js 	deleted:    frontend/src/setupTests.js

--- frontend/src/pages/MainPage/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const MainWrapper = styled.div`
-  background-color: aqua;
-
-  p {
-    color: red;
-  }
-`;
--- frontend/src/pages/OrderPage/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const OrderWrapper = styled.div`
-  background-color: beige;
-
-  p {
-    color: yellow;
-  }
-`;
