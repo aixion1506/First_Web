f9e3fd5f02616b42605b792ff74c0a8e9a599564
[AI6/1반] 조해정 <hj6elice@gmail.com>
2023-02-04 02:23:31 +0900

    patch: class로 수정

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,77 +1,77 @@
-import {
-  addProductService,
-  getProductsService,
-  getProductsByCategoryService,
-  getProductDataService,
-  deleteProductService,
-} from "../services/productService";
+import { productService } from "../services";
 
-export const addProduct = async (req, res, next) => {
-  try {
-    const {
-      title,
-      categoryId,
-      manufacturer,
-      shortDescription,
-      detailDescription,
-      imageKey,
-      inventory,
-      price,
-      searchKeywords,
-    } = req.body;
+class ProductController {
+  async addProduct(req, res, next) {
+    try {
+      const {
+        title,
+        categoryId,
+        manufacturer,
+        shortDescription,
+        detailDescription,
+        imageKey,
+        inventory,
+        price,
+        searchKeywords,
+      } = req.body;
 
-    const newProduct = await addProductService({
-      title,
-      categoryId,
-      manufacturer,
-      shortDescription,
-      detailDescription,
-      imageKey,
-      inventory,
-      price,
-      searchKeywords,
-    });
-    res.status(201).json(newProduct);
-  } catch (err) {
-    next(err);
+      const newProduct = await productService.addProduct({
+        title,
+        categoryId,
+        manufacturer,
+        shortDescription,
+        detailDescription,
+        imageKey,
+        inventory,
+        price,
+        searchKeywords,
+      });
+      res.status(201).json(newProduct);
+    } catch (err) {
+      next(err);
+    }
   }
-};
 
-export const getProducts = async (req, res, next) => {
-  try {
-    const products = await getProductsService();
-    res.status(200).json(products);
-  } catch (err) {
-    next(err);
+  async getProducts(req, res, next) {
+    try {
+      const products = await productService.getProducts();
+      res.status(200).json(products);
+    } catch (err) {
+      next(err);
+    }
   }
-};
 
-export const getProductsByCategory = async (req, res, next) => {
-  const title = req.params.categoryTitle;
-  try {
-    const products = await getProductsByCategoryService(title);
-    res.status(200).json(products);
-  } catch (err) {
-    next(err);
+  async getProductsByCategory(req, res, next) {
+    const title = req.params.categoryTitle;
+    try {
+      const products = await productService.getProductsByCategory(title);
+      res.status(200).json(products);
+    } catch (err) {
+      next(err);
+    }
   }
-};
 
-export const getProductData = async (req, res, next) => {
-  const title = req.params.productTitle;
-  try {
-    const productData = await getProductDataService(title);
-    res.status(200).json(productData);
-  } catch (err) {
-    next(err);
+  async getProduct(req, res, next) {
+    const title = req.params.productTitle;
+    try {
+      const productData = await productService.getProduct(title);
+      res.status(200).json(productData);
+    } catch (err) {
+      next(err);
+    }
   }
-};
 
-export const deleteProduct = async (req, res, next) => {
-  const title = req.params.productTitle;
-  try {
-    const result = await deleteProductService(title);
-    res.status(200).json(result);
-  } catch (err) {
-    next(err);
+  async deleteProduct(req, res, next) {
+    const title = req.params.productTitle;
+    try {
+      const result = await productService.deleteProduct(title);
+      res.status(200).json(result);
+    } catch (err) {
+      next(err);
+    }
   }
-};
+}
+
+const productController = new ProductController();
+
+export { productController };
--- backend/services/categoryService.js
+++ backend/services/categoryService.js
@@ -1,43 +1,59 @@
 import { Product, Category } from "../models";
 
-// 카테고리 추가
-export const addCategoryService = async categoryInfo => {
-  const { title } = categoryInfo;
-
-  // 카테고리 중복 확인
-  const founded = await Category.findOne({ title });
-  if (founded) {
-    throw new Error("이미 존재하는 카테고리입니다.");
+class CategoryService {
+  constructor(productModel, categoryModel) {
+    this.productModel = productModel;
+    this.categoryModel = categoryModel;
   }
 
-  const createdNewCategory = await Category.create(categoryInfo);
-  return createdNewCategory;
-};
+  // 카테고리 추가
+  async addCategory(categoryInfo) {
+    const { title } = categoryInfo;
 
-// 카테고리 목록 조회
-export const getCategoriesService = async () => {
-  const categories = await Category.find({});
-  return categories;
-};
+    // 카테고리 중복 확인
+    const founded = await this.categoryModel.findOne({ title });
+    if (founded) {
+      throw new Error("이미 존재하는 카테고리입니다.");
+    }
 
-// 카테고리 수정
-// 카테고리 삭제
-export const deleteCategoryService = async title => {
-  const category = await Category.findOne({ title });
-  if (!category) {
-    throw new Error("해당 카테고리가 없습니다.");
+    const createdNewCategory = await this.categoryModel.create(categoryInfo);
+    return createdNewCategory;
   }
 
-  const product = await Product.findOne({ categoryId: category.id });
-  if (product) {
-    throw new Error("해당 카테고리에 제품이 있어 삭제가 불가합니다.");
+  // 카테고리 목록 조회
+  async getCategories() {
+    const categories = await this.categoryModel.find({});
+    return categories;
   }
 
-  const { deletedCount } = await Category.deleteOne({ _id: category.id });
-  if (deletedCount === 0) {
-    throw new Error("해당 카테고리 삭제에 실패했습니다.");
-  }
+  // 카테고리 수정
+  // aysnc setCategory(~) => {};
 
-  return { result: "success" };
-};
-// 특정 카테고리 조회
+  // 카테고리 삭제
+  async deleteCategory(title) {
+    const category = await this.categoryModel.findOne({ title });
+    if (!category) {
+      throw new Error("해당 카테고리가 없습니다.");
+    }
+
+    const product = await this.productModel.findOne({
+      categoryId: category.id,
+    });
+    if (product) {
+      throw new Error("해당 카테고리에 제품이 있어 삭제가 불가합니다.");
+    }
+
+    const { deletedCount } = await this.categoryModel.deleteOne({
+      _id: category.id,
+    });
+    if (deletedCount === 0) {
+      throw new Error("해당 카테고리 삭제에 실패했습니다.");
+    }
+
+    return { result: "success" };
+  }
+}
+
+const categoryService = new CategoryService(Product, Category);
+
+export { categoryService };
