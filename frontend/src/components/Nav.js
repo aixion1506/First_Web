062af7e046a9c62153720a831879910269df7c4c
unysmins <unysmins@naver.com>
2023-02-06 14:39:09 +0900

    feat: header, nav styled file create

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
-import styled from "styled-components";
-import { Link } from "react-router-dom";
+import {NavContainer, LinkStyle} from "./nav-styled";
 import DropDown from "./DropDown";
 const Nav = () => {
   const [flag, setFlag] = useState(false);
@@ -12,35 +11,10 @@ const Nav = () => {
               <span>SHOP</span>
                 {flag && <DropDown />}
             </li>
-            <li><LinkStyle to="/">COLLECTION</LinkStyle></li>
-            <li><LinkStyle to="/">ABOUT</LinkStyle></li>
-            <li><LinkStyle to="/">BOARD</LinkStyle></li>
           </ul>
       </NavContainer>
     </>
   );
 };
-
-const NavContainer = styled.div`
-  position: fixed;
-  width: 300px;
-  height: 100%;
-  padding: 110px 40px;
-  box-sizing: border-box;
-
-  > ul > li:not(:last-of-type) {
-    margin-bottom: 20px;
-  }
-
-  span {
-    cursor: pointer;
-  }
-`;
-
-const LinkStyle = styled(Link)`
-  display: inline-block;
-  color: #000;
-  text-decoration: none;
-`;
 
 export default Nav;
