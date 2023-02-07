9ecf2a1bc385cc2ba5b543c55ffedf5b632671eb
kshoon <kshoon0518@gmail.com>
2023-02-07 10:33:47 +0900

    patch: 주문 API 코드 수정

--- backend/routes/orderProductRouter.js
+++ backend/routes/orderProductRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import orderProductController from "../controllers/orderProductController";
+import { orderProductController } from "../controllers/orderProductController";
 
 const orderProductRouter = express.Router();
 
