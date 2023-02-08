05cd99c0274211e60f0b95a8ae42348b799c34b1
kshoon <kshoon0518@gmail.com>
2023-02-08 11:55:55 +0900

    patch: git 오류 수정

--- backend/backend/middleware/errorHandler404.js
+++
@@ -1,7 +0,0 @@
-import createError from "http-errors";
-
-function errorHandler404(req, res, next) {
-  next(createError(404));
-}
-
-export default errorHandler404;
--- backend/backend/middleware/errorLogger.js
+++
@@ -1,8 +0,0 @@
-import logger from "./logger";
-
-function errorLogger(err, req, res, next) {
-  logger.error("Error 로그", { message: err });
-  next(err);
-}
-
-export default errorLogger;
